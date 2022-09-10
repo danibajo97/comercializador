d77115480c4b031aff428841b7e89171cf40b245
Yoelvys Pérez Cabrera <yoelvys.perez@datazucar.cu>
2022-09-10 08:36:32 -0700

    configuracion del provider ToastContainer para las notificaciones

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { CustomProvider } from 'rsuite'
 import esES from 'rsuite/locales/es_ES'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
 
 import { store } from './redux/store'
 import Template from './Template'
@@ -11,10 +12,11 @@ import Template from './Template'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'rsuite/dist/rsuite.min.css'
 
-import 'assets/vendor/nucleo/css/nucleo.css'
-import 'assets/vendor/futura/css/futura.css'
 import 'assets/vendor/fortawesome/fontawesome-free/css/all.min.css'
 import 'assets/css/argon-dashboard-react.css'
+import 'assets/vendor/nucleo/css/nucleo.css'
+import 'assets/vendor/futura/css/futura.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -23,6 +25,17 @@ root.render(
     <BrowserRouter>
       <CustomProvider locale={esES}>
         <Template />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </CustomProvider>
     </BrowserRouter>
   </Provider>
