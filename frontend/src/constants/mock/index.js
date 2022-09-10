cc1460d541fa95c965e499e6c4b2caacf749dd39
Yoelvys Pérez Cabrera <yoelvys.perez@datazucar.cu>
2022-09-10 08:34:44 -0700

    mock para las formularios

---
+++ frontend/src/constants/mock/index.js
@@ -0,0 +1,6 @@
+import clienteFinal from './mockClienteFinal'
+import buscarContrato from './mockBuscarContrato'
+import facturarseA from './mockFacturarseA'
+import servicioContratado from './mockServicioContratado'
+
+export { clienteFinal, buscarContrato, facturarseA, servicioContratado }
---
+++ frontend/src/constants/mock/mockFacturarseA.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 'e7c76931-93a1-469a-891f-91c2dc81ef07',
+    contacto_cliente: 'UEB DESOFT VC',
+    representante_cliente: null,
+    abreviatura_cliente: 'UEB DESOFT VC',
+    tipo_entidad_cliente: 'Empresa',
+    organismo_cliente: 'Ministerio de Comunicaciones',
+    codigo_cliente: null,
+    telefono_cliente: '',
+    correo_cliente: '',
+    entidad_cliente: 'EMPRESA PILOTO',
+    pais_cliente: 'Cuba',
+    provincia_cliente: 'Villa Clara',
+    municipio_cliente: 'Santa Clara',
+    direccion_cliente: '',
+    contacto_id: 'f8a95b2b-a037-5d67-a126-c70657685274'
+  },
+  {
+    id: '00265f59-044d-4d97-a03e-0bfd957b90b7',
+    contacto_cliente: 'UEB DESOFT SS',
+    representante_cliente: null,
+    abreviatura_cliente: 'UEB DESOFT SS',
+    tipo_entidad_cliente: 'Empresa',
+    organismo_cliente: 'Ministerio de Comunicaciones',
+    codigo_cliente: '897878789789780',
+    telefono_cliente: '',
+    correo_cliente: '',
+    entidad_cliente: 'EMPRESA PILOTO',
+    pais_cliente: 'Cuba',
+    provincia_cliente: 'Sancti Sp\u00edritus',
+    municipio_cliente: 'Sancti Sp\u00edritus',
+    direccion_cliente: '',
+    contacto_id: '12751d70-b887-5713-97b7-5674156e4b03'
+  }
+]
