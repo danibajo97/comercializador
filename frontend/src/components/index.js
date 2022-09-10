1931a060ba28d828062471face3861f4f8f44095
Yoelvys Pérez Cabrera <yoelvys.perez@datazucar.cu>
2022-09-10 08:40:19 -0700

    nuevos ajustes en los componentes y nuevos creados

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,5 +4,7 @@ import Navbar from 'components/layout/Navbar'
 import Sidebar from 'components/layout/Sidebar'
 import DefaultHeader from 'components/headers/DefaultHeader'
 import Header from 'components/headers/Header'
+import Table from 'components/table/Table'
+import FormField, { Textarea, InputNumber } from 'components/form/FormField'
 
-export { Button, Footer, Navbar, Sidebar, Header, DefaultHeader }
+export { Button, Footer, Navbar, Sidebar, Header, DefaultHeader, FormField, Textarea, InputNumber, Table }
