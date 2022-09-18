b39d42e2283372336f433600d8040ccee1cc8f90
Yoelvys Pérez Cabrera <yoelvys.perez@datazucar.cu>
2022-09-18 14:31:46 -0700

    se crearon constantes para los mock de la informacion

--- frontend/src/constants/mock/index.js
+++ frontend/src/constants/mock/index.js
@@ -2,5 +2,6 @@ import clienteFinal from './mockClienteFinal'
 import buscarContrato from './mockBuscarContrato'
 import facturarseA from './mockFacturarseA'
 import servicioContratado from './mockServicioContratado'
+import convenios from './mockConvenios'
 
-export { clienteFinal, buscarContrato, facturarseA, servicioContratado }
+export { clienteFinal, buscarContrato, facturarseA, servicioContratado, convenios }
