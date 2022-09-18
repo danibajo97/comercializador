d60ba214033cbf69f43ba8e78f29b3be06d69317
Yoelvys Pérez Cabrera <yoelvys.perez@datazucar.cu>
2022-09-18 14:33:43 -0700

    nueva vista de login y registro de usuario

---
+++ frontend/src/views/login/Register.js
@@ -0,0 +1,75 @@
+import React, { useEffect } from 'react'
+import {
+  Card,
+  CardBody,
+  Row,
+  Col,
+  Container
+} from 'reactstrap'
+import { useNavigate } from 'react-router-dom'
+import { Form, Button } from 'rsuite'
+
+import { DefaultHeader, FormField, InputPassword } from 'components'
+
+export default function Register ({ setHeaderVisible }) {
+  const navigate = useNavigate()
+
+  useEffect(() => { setHeaderVisible(false) }, [])
+
+  const irInicioSesion = evt => {
+    evt.preventDefault()
+    navigate('/login')
+  }
+
+  return (
+    <>
+      <DefaultHeader height={7} />
+      <Container className='mt--7' fluid>
+        <Row>
+          <Col xs='12' sm='12' md='8' lg='7' xl='6' xxl='5' className='center'>
+            <Card className='bg-secondary shadow p-3'>
+              <CardBody>
+                <Col>
+                  <div className='text-center text-muted mb-5'>
+                    <h3>Comercializador - Datazucar</h3>
+                    <small>Crear una nueva cuenta</small>
+                  </div>
+                  <Form fluid>
+                    <Row>
+                      <Col xs='12' sm='6' className='mb-4'>
+                        <FormField name='firstname' label='Nombre' required />
+                        <FormField name='username' label='Usuario' required />
+                        <InputPassword name='password' label='Contraseña' required />
+                      </Col>
+                      <Col xs='12' sm='6'>
+                        <FormField name='lastname' label='Apellidos' required />
+                        <FormField name='email' label='Correo' required />
+                        <InputPassword name='re_password' label='Repetir Contraseña' required />
+                      </Col>
+                    </Row>
+                    <div className='text-center'>
+                      <Button className='mt-4' appearance='primary' size='sm'>
+                        Crear Cuenta
+                      </Button>
+                    </div>
+                  </Form>
+                </Col>
+              </CardBody>
+            </Card>
+            <Row className='mt-3'>
+              <Col className='text-center'>
+                <a
+                  className='text-muted'
+                  href='#pablo'
+                  onClick={irInicioSesion}
+                >
+                  <small>Ir al Inicia de Sesión</small>
+                </a>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
