9445e760d986355a0da349b12fe9d81ab9a08895
Yoelvys Pérez Cabrera <yoelvys.perez@datazucar.cu>
2022-09-26 11:30:27 -0700

    nuevas constantes

---
+++ frontend/src/constants/mock/mockPlazosPago.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: '7aad845a-a666-4bf5-b631-b4b3e1a66aba',
+    fecha: '2022-09-20',
+    importe: 540,
+    facturado: false,
+    cobrado: false
+  },
+  {
+    id: '7aad845a-a666-4bf5-b631-b4b3e1a66aaa',
+    fecha: '2022-09-21',
+    importe: 245,
+    facturado: true,
+    cobrado: false
+  },
+  {
+    id: '7aad845a-a666-4bf5-b631-b4b3e1a66abb',
+    fecha: '2022-09-22',
+    importe: 840,
+    facturado: false,
+    cobrado: true
+  }
+]
