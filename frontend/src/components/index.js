27fba216a5bb6532737499c199cbacf5b910816d
Yoelvys Pérez Cabrera <yoelvys.perez@datazucar.cu>
2022-09-26 11:28:45 -0700

    Componente Table

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,8 +3,7 @@ import Footer from 'components/layout/Footer'
 import Navbar from 'components/layout/Navbar'
 import Sidebar from 'components/layout/Sidebar'
 import DefaultHeader from 'components/headers/DefaultHeader'
-import Header from 'components/headers/Header'
-import Table, { renderEmpty } from 'components/table/Table'
+import { renderEmpty } from 'components/table/Table'
 import FormField from 'components/form/FormField'
 import Textarea from 'components/form/Textarea'
 import InputPassword from 'components/form/InputPassword'
@@ -15,12 +14,10 @@ export {
   Footer,
   Navbar,
   Sidebar,
-  Header,
   DefaultHeader,
   FormField,
   Textarea,
   InputNumber,
   InputPassword,
-  Table,
   renderEmpty
 }
