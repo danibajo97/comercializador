250e5111adcc8aa84233dd72f9ddbbc1d2acccf0
Daniel Izquierdo Hernandez <daniel.izquierdo@datazucar.cu>
2022-10-13 13:04:57 -0400

    Servicio Contratado

--- apps/convenio/api/views/servicio_contratado_views.py
+++ apps/convenio/api/views/servicio_contratado_views.py
@@ -7,50 +7,52 @@ from apps.users.resources.authenticated_user import authenticated_user
 
 
 class ServicioContratadoViewSet(viewsets.GenericViewSet):
+    responsebase = ResponseBase()
 
     @transaction.atomic
     def create(self, request):
-        url = 'http://127.0.0.1:8000/cmz/servicio_contratado_externo/add-servicio'
-        response = requests.post(url, json=request.data)
+        user = authenticated_user(request)
+        url = 'cmz/servicio_contratado_externo/'
+        params = {
+            'authenticated-user': user.id_erp,
+        }
+        response = self.responsebase.post(
+            url=url, json=request.data, params=params)
         if response.status_code == 201:
-
-            return Response(response.request.data, status=response.status_code)
+            return Response({'Comercializador-response': 'Creado correctamente',
+                             'Versat-response': response.json()}, status=response.status_code)
         else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
+            return Response({'Versat-response': response.json()},
                             status=response.status_code)
 
     def list(self, request):
-
-        url = 'http://127.0.0.1:8000/cmz/servicio_contratado_externo/lista-servicios'
-        response = requests.get(url, request)
-
-        if response.status_code == 200:
-            return Response(response.json(), status=response.status_code)
+        user = authenticated_user(request)
+        if request.GET.get('id_servicio_contratado'):
+            url = '%s%s/' % ('cmz/servicio_contratado_externo/',
+                             request.GET.get('id_servicio_contratado'))
         else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
-                            status=response.status_code)
+            url = 'cmz/servicio_contratado_externo/'
+        params = {
+            'authenticated-user': user.id_erp,
+        }
+        response = self.responsebase.get(url=url, params=params)
+        return Response({'Versat-response': response.json()},
+                        status=response.status_code)
 
     @transaction.atomic
-    def update(self, request):
-
-        url = 'http://127.0.0.1:8000/cmz/servicio_contratado_externo/'
-        response = requests.patch(url, request)
-
-        if response.status_code == 204:
-            return Response(status=response.status_code)
-        else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
-                            status=response.status_code)
-
-    def retrieve(self, request):
-
-        url = 'http://127.0.0.1:8000/cmz/servicio_contratado_externo/' + request.GET.get('id_servicio_contratado')
-        response = requests.get(url)
-
+    def put(self, request, pk):
+        user = authenticated_user(request)
+        url = '%s%s/' % ('cmz/servicio_contratado_externo/', pk)
+        params = {
+            'authenticated-user': user.id_erp,
+        }
+        response = self.responsebase.put(
+            url=url, params=params, json=request.data)
         if response.status_code == 200:
-            return Response(response.json(), status=response.status_code)
+            return Response({'Comercializador-response': 'Actualizado Correctamente',
+                             'Versat-response': response.json()}, status=response.status_code)
         else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
+            return Response({'Versat-response': response.json()},
                             status=response.status_code)
 
     @transaction.atomic
