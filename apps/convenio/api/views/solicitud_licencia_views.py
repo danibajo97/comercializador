767ffc3848907c7c576550d051ff686036b2529f
Daniel Izquierdo Hernandez <daniel.izquierdo@datazucar.cu>
2022-10-14 16:11:13 -0400

    solicitud licencia

--- apps/convenio/api/views/solicitud_licencia_views.py
+++ apps/convenio/api/views/solicitud_licencia_views.py
@@ -1,123 +1,76 @@
 from django.db import transaction
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from apps.base.response_base import ResponseBase
 from apps.users.resources.authenticated_user import authenticated_user
 
 
-class SolicitudLicenciaWebViewSet(viewsets.GenericViewSet):
+class SolicitudLicenciaViewSet(viewsets.GenericViewSet):
     responsebase = ResponseBase()
 
     @transaction.atomic
     def create(self, request):
-        url = 'cmz/convenio_externo/'
-        response = self.responsebase.post(url=url, json=request.data)
+        user = authenticated_user(request)
+        url = 'cmz/solicitud_licencia_venta_externo/'
+        params = {
+            'authenticated-user': user.id_erp,
+        }
+        response = self.responsebase.post(
+            url=url, json=request.data, params=params)
         if response.status_code == 201:
-            # serializer = ConvenioWebSerializer(data=response.json())
-            # serializer.is_valid(raise_exception=True)
-            # serializer.save()
-            return Response(response.json(), status=response.status_code)
+            return Response({'Comercializador-response': 'Creado correctamente',
+                             'Versat-response': response.json()}, status=response.status_code)
         else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
+            return Response({'Versat-response': response.json()},
                             status=response.status_code)
 
     def list(self, request):
         user = authenticated_user(request)
-        url = 'cmz/convenio_externo/'
+        if request.GET.get('id_solicitud_licencia_venta'):
+            url = '%s%s/' % ('cmz/solicitud_licencia_venta_externo/',
+                             request.GET.get('id_solicitud_licencia_venta'))
+        else:
+            url = 'cmz/solicitud_licencia_venta_externo/'
         params = {
-            'id_contacto': user.id_erp,
+            'authenticated-user': user.id_erp,
         }
         response = self.responsebase.get(url=url, params=params)
+        return Response(response.json(), status=response.status_code)
+
+    @transaction.atomic
+    def update(self, request, pk):
+        user = authenticated_user(request)
+        params = {
+            'authenticated-user': user.id_erp,
+        }
+        url = 'cmz/solicitud_licencia_venta_externo/%s/' % pk
+        response = self.responsebase.put(
+            url=url, json=request.data, params=params)
         if response.status_code == 200:
-            return Response(response.json(), status=response.status_code)
+            return Response({'Comercializador-response': 'Actualizado Correctamente',
+                             'Versat-response': response.json()}, status=response.status_code)
         else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
+            return Response({'Versat-response': response.json()},
                             status=response.status_code)
 
     @transaction.atomic
-    def update(self, request, pk=None):
-        pass
-
-    def retrieve(self, request, pk=None):
-        url = 'cmz/convenio_externo/' + request.GET.get('id_convenio')
+    def retrieve(self, request, pk):
+        url = 'cmz/solicitud_licencia_venta_externo/%s/' % pk
         response = self.responsebase.get(url=url)
         if response.status_code == 200:
             return Response(response.json(), status=response.status_code)
         else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
+            return Response({'Versat-response': response.json()},
                             status=response.status_code)
 
     @transaction.atomic
-    def delete(self, request, pk=None):
-        url = 'cmz/convenio_externo/' + request.GET.get('id_convenio'),
+    def destroy(self, request, pk):
+        url = 'cmz/solicitud_licencia_venta_externo/%s/' % pk
         response = self.responsebase.delete(url=url)
         if response.status_code == 204:
-            # convenio = ConvenioWeb.objects.filter(
-            #     name=request.data['id_convenio']).first()
-            # convenio.delete()
-            return Response(status=response.status_code)
-        else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
+            return Response({'Comercializador-response': 'Eliminado correctamente'},
                             status=response.status_code)
-
-    @action(detail=False, methods=['get'])
-    def usuarios_finales(self, request):
-        user = authenticated_user(request)
-        params = {
-            'idcontacto': user.id_erp,
-        }
-        url = 'cmz/convenio_externo/usuarios_finales/'
-        response = self.responsebase.get(url=url, params=params)
-        if response.status_code == 200:
-            return Response(response.json(), status=response.status_code)
         else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
-                            status=response.status_code)
-
-    @action(detail=False, methods=['get'])
-    def list_servicios(self, request):
-        user = authenticated_user(request)
-        params = {
-            'idcontacto': user.id_erp,
-            'idplazopago': request.GET.get('id_plazopago'),
-            'idconvenio': request.GET.get('id_convenio'),
-        }
-        url = 'cmz/convenio_externo/list_servicios/'
-        response = self.responsebase.get(url=url, params=params)
-        if response.status_code == 200:
-            return Response(response.json(), status=response.status_code)
-        else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
-                            status=response.status_code)
-
-    @action(detail=False, methods=['put'])
-    def validar_convenio(self, request):
-        user = authenticated_user(request)
-        params = {
-            'idcontacto': user.id_erp,
-            'idconvenio': request.GET.get('id_convenio'),
-        }
-        url = 'cmz/convenio_externo/validar_convenio/'
-        response = self.responsebase.get(url=url, params=params)
-        if response.status_code == 200:
-            return Response(response.json(), status=response.status_code)
-        else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
-                            status=response.status_code)
-
-    @action(detail=False, methods=['put'])
-    def confirmar_convenio(self, request):
-        user = authenticated_user(request)
-        params = {
-            'idcontacto': user.id_erp,
-            'idconvenio': request.GET.get('id_convenio'),
-        }
-        url = 'cmz/convenio_externo/confirmar_convenio/'
-        response = self.responsebase.get(url=url, params=params)
-        if response.status_code == 200:
-            return Response(response.json(), status=response.status_code)
-        else:
-            return Response({'message': "Hubo problemas al conectar con el servidor"},
+            return Response({'Versat-response': response.json()},
                             status=response.status_code)
