13bb3dd2a11e6ce4ebbe86aea1332331f92cf5dc
Daniel Izquierdo Hernandez <daniel.izquierdo@datazucar.cu>
2022-11-11 16:37:41 -0500

    detalle

--- apps/users/api/serializers/users_serializers.py
+++ apps/users/api/serializers/users_serializers.py
@@ -13,18 +13,12 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'name', 'last_name')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
+        fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
-            first_name=validated_data['name'],
-            last_name=validated_data['last_name']
         )
 
         user.set_password(validated_data['password'])
