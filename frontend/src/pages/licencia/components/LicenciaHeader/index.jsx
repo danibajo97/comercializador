e924eb4f436e44f1ed1251cefb5513cbfe98e6fe
Daniel Izquierdo Hernandez <daniel.izquierdo@datazucar.cu>
2022-11-24 11:37:01 -0500

    arreglando conflictos

--- frontend/src/pages/licencia/components/LicenciaHeader/index.jsx
+++ frontend/src/pages/licencia/components/LicenciaHeader/index.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import useAuth from 'hooks/useAuth'
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
@@ -36,6 +37,45 @@ const ConvenioHeader = ({ totalLicencia, totalOtorgada, totalPendiente }) => {
 }
 
 export default ConvenioHeader
+=======
+import useAuth from 'hooks/useAuth'
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+
+import { CardHeader } from 'components'
+
+const ConvenioHeader = ({ totalLicencia, totalOtorgada, totalPendiente }) => {
+  const { user } = useAuth()
+
+  const col = { xl: 3, lg: 6, md: 6, sm: 6, xs: 12 }
+  return (
+    <>
+      <div className='header bg-gradient-info pb-8 pt-7 pt-md-8'>
+        <Container fluid>
+          <div className='header-body'>
+            <Row>
+              <Col {...col}>
+                <CardHeader title='Cliente' value={user?.distribuidor?.nombre || 'Cargando...'} icon='user' color='info' />
+              </Col>
+              <Col {...col}>
+                <CardHeader title='Total de licencias' value={totalLicencia || 0} icon='id-card' color='blue' />
+              </Col>
+              <Col {...col}>
+                <CardHeader title='Otorgada' value={totalOtorgada || 0} icon='check-square' color='success' />
+              </Col>
+              <Col {...col}>
+                <CardHeader title='Pendiente' value={totalPendiente || 0} icon='edit' color='warning' />
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default ConvenioHeader
+>>>>>>> b9e22348c3fbfc0ac0d804ce6a9284166f55da50
 =======
 import useAuth from 'hooks/useAuth'
 import React from 'react'
