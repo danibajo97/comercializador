b34c21eac01824d75cec30412ead741599a97327
yoelvys <yoelvys.perez@datazucar.cu>
2022-12-06 09:41:27 -0500

    fix(convenio): error al asociar los clientes

--- frontend/src/pages/convenio/PlazosPago/AsociarServiciosForm/index.jsx
+++ frontend/src/pages/convenio/PlazosPago/AsociarServiciosForm/index.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Col, Form, Button, ButtonToolbar, Schema, Divider, CheckPicker, SelectPicker } from 'rsuite'
+import { Col, Form, ButtonToolbar, Schema, Divider, CheckPicker, SelectPicker, Message } from 'rsuite'
 
-import { FormField, InputNumber, Loader } from 'components'
+import { FormField, Loader, Button } from 'components'
 import Table from 'components/table/Table'
 import { getPlazoPagoServicioAll, addPlazoPagoServicio, updatePlazoPagoServicio, stateResetOperation as stateResetOperationPlazoPagoServicio } from 'redux/plazoPagoServicio/plazoPagoServicioSlice'
 import { getClientesFinales, stateResetOperation as stateResetOperationClientesFinales } from 'redux/clientesFinales/clientesFinalesSlice'
 import { getServiciosContratadosAll, stateResetOperation as stateResetOperationServiciosContratados } from 'redux/serviciosContratados/serviciosContratadosSlice'
 import OPERATIONS from 'constants/operationsRedux'
+import { toast } from 'react-toastify'
 
 export function AsociarServiciosForm ({ closeModal, convenioId, plazoPagoId, servicioAsociado = null }) {
   const [cantidadBD, setCantidadBD] = useState(null)
-  const formRef = React.useRef()
+  const formRef = useRef()
   const dispatch = useDispatch()
 
   const clientesFinales = useSelector(state => state.clientesFinales.clientesFinales)
@@ -22,7 +23,7 @@ export function AsociarServiciosForm ({ closeModal, convenioId, plazoPagoId, ser
   const isAdd = useSelector(state => state.plazoPagoServicio.isAdd)
   const isUpdate = useSelector(state => state.plazoPagoServicio.isUpdate)
 
-  React.useEffect(() => {
+  useEffect(() => {
     dispatch(getClientesFinales({ convenio: convenioId }))
     dispatch(getServiciosContratadosAll({ convenio: convenioId }))
 
@@ -33,33 +34,29 @@ export function AsociarServiciosForm ({ closeModal, convenioId, plazoPagoId, ser
     }
   }, [])
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (servicioAsociado !== null && serviciosContratados.length > 0) {
       const serviciosContratadosFind = serviciosContratados.find(sc => sc.servicio === servicioAsociado.servicio)
       setCantidadBD(serviciosContratadosFind.cantidad)
     }
   }, [serviciosContratados])
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (isAdd === OPERATIONS.FULFILLED || isUpdate === OPERATIONS.FULFILLED) {
       dispatch(getPlazoPagoServicioAll({ plazoPagoId }))
       if (closeModal) closeModal()
     }
   }, [isAdd, isUpdate])
 
-  const [formValue, setFormValue] = React.useState({
+  const [formValue, setFormValue] = useState({
     servicios: servicioAsociado?.servicio,
     cantidad: servicioAsociado?.cantidad,
     clientesAsociados: servicioAsociado?.usuariosfinales
   })
 
-  const { NumberType, StringType, ArrayType } = Schema.Types
+  const { StringType, ArrayType } = Schema.Types
   const model = Schema.Model({
     servicios: StringType()
-      .isRequired('Este campo es obligatorio.'),
-    cantidad: NumberType()
-      .min(1, 'No puede ser menor que 1.')
-      .max(cantidadBD || 1, `No puede ser mayor que ${cantidadBD || 1}.`)
       .isRequired('Este campo es obligatorio.'),
     clientesAsociados: ArrayType()
       .isRequired('Este campo es obligatorio.')
@@ -71,19 +68,12 @@ export function AsociarServiciosForm ({ closeModal, convenioId, plazoPagoId, ser
     }).filter(data => data !== undefined)
   }
 
-  const onChangeClienteFinal = (value, event) => {
-    const count = cantidadBD || 1
-    if (value.length <= count) {
-      setFormValue({
-        ...formValue,
-        clientesAsociados: value,
-        cantidad: value.length
-      })
-    }
-  }
-
   const handleSubmit = () => {
     if (formRef.current.check()) {
+      if (formValue.cantidad !== cantidadBD) {
+        toast.error('No coinciden las base de datos.')
+        return
+      }
       const params = {
         cantidad: formValue.cantidad,
         plazo: plazoPagoId,
@@ -100,62 +90,100 @@ export function AsociarServiciosForm ({ closeModal, convenioId, plazoPagoId, ser
 
   const onSelectServicio = (value, item, event) => {
     setCantidadBD(item.cantidad)
+    setFormValue({
+      ...formValue,
+      servicios: value
+    })
+  }
+
+  const onChangeClienteFinal = (value, event) => {
+    const count = cantidadBD || 1
+    if (value.length <= count) {
+      setFormValue({
+        ...formValue,
+        clientesAsociados: value,
+        cantidad: value.length
+      })
+    }
   }
 
   const renderForm = () => (
     <Form
       fluid
       ref={formRef}
-      onChange={setFormValue}
+      // TODO: onChange={setFormValue} se quito el evento para controlar la cantidad de clientes a seleccionar
       formValue={formValue}
       model={model}
     >
-      <Col xs={24} sm={24} md={12} lg={12} className='mb-4'>
+      <Col xs={24} sm={24} md={24} lg={24} className='mb-2'>
         <FormField
-          name='servicios' label='Servicios' accepter={SelectPicker} required block onSelect={onSelectServicio} data={serviciosContratados.map(item => ({
+          name='servicios'
+          label='Servicios'
+          accepter={SelectPicker}
+          required block
+          cleanable={false}
+          onSelect={onSelectServicio}
+          data={serviciosContratados.map(item => ({
             label: item.producto_nombre,
             value: item.servicio,
             cantidad: item.cantidad
           }))}
         />
       </Col>
+      {/* Todo: este campo es solo de informacion
       <Col xs={24} sm={24} md={12} lg={12}>
-        <FormField name='cantidad' label='Cantidad a Incluir' accepter={InputNumber} required disabled />
+        <FormField name='cantidad' label='Cantidad a Incluir' accepter={InputNumber} required readOnly />
       </Col>
-      <Col xs={24}>
-        <Divider />
-        <h6 className='heading-small text-muted mb-4'>
-          Clientes a Asociar
-        </h6>
-      </Col>
-      <Col xs={24}>
-        <FormField
-          name='clientesAsociados'
-          label='Clientes'
-          accepter={CheckPicker}
-          onChange={onChangeClienteFinal}
-          required block data={clientesFinales.map(item => ({
-            label: item.nombre,
-            value: item.id
-          }))}
-        />
-      </Col>
-      <Col xs={24} className='mt-4'>
-        {tableData().length > 0 &&
-          <Table data={tableData()} autoHeight>
-            {Table.Column({ header: 'Nombre Completo Cliente', dataKey: 'nombre', flex: 1, white: true })}
-            {Table.Column({ header: 'Correo', dataKey: 'correo', flex: 1, white: true })}
-          </Table>}
-      </Col>
+      */}
+      {formValue.servicios && (
+        <>
+          <Col xs={24}>
+            <Divider />
+            <h6 className='heading-small text-muted mb-4'>
+              Clientes a Asociar
+            </h6>
+          </Col>
+          <Col xs={24}>
+            <Message showIcon style={{ backgroundColor: '#E3F3FD' }} className='mb-4 ml--1 mr--1'>
+              El servicio seleccionado corresponde a {cantidadBD} base de dato{cantidadBD !== 1 && 's'}.
+            </Message>
+          </Col>
+          <Col xs={24}>
+            <FormField
+              name='clientesAsociados'
+              label='Clientes'
+              accepter={CheckPicker}
+              onChange={onChangeClienteFinal}
+              required block data={clientesFinales.map(item => ({
+                label: item.nombre,
+                value: item.id
+              }))}
+            />
+          </Col>
+          <Col xs={24} className='mt-4'>
+            {tableData().length > 0 &&
+              <Table data={tableData()} autoHeight>
+                {Table.Column({ header: 'Nombre Completo Cliente', dataKey: 'nombre', flex: 1, white: true })}
+                {Table.Column({ header: 'Correo', dataKey: 'correo', flex: 1, white: true })}
+              </Table>}
+          </Col>
+        </>)}
       <Col xs={24} className='mt-4'>
         <ButtonToolbar>
-          <Button appearance='primary' size='sm' onClick={handleSubmit}>
-            Guardar
-          </Button>
+          <Button
+            icon='save'
+            text='Guardar'
+            appearance='primary'
+            onClick={handleSubmit}
+          />
           {closeModal &&
-            <Button appearance='subtle' color='red' size='sm' onClick={closeModal}>
-              Cerrar
-            </Button>}
+            <Button
+              icon='times'
+              text='Cerrar'
+              appearance='subtle'
+              color='red'
+              onClick={closeModal}
+            />}
         </ButtonToolbar>
       </Col>
     </Form>
