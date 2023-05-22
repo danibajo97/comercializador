5a58961c216eaf79008c29c0b8dc4f3c2c3e8f64
Daniel Izquierdo Hernandez <daniel.izquierdo@datazucar.cu>
2023-05-22 17:22:22 -0400

    Cambio de BD para postgres

--- comercializador/settings.py
+++ comercializador/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from datetime import timedelta
+from pathlib import Path
 
 from decouple import config
-from pathlib import Path
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'comercializador.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'comercializador',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
